17f6e59930e0087bace0b9ad9dce6cb24519e72a
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-05-02 17:44:52 +0700

    Thêm modal thêm nhuưng chưa len

--- src/app/pages/system/branch/branch.component.ts
+++ src/app/pages/system/branch/branch.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { SystemService } from 'src/app/core/services/system.service';
 import { DEFAULT_PER_PAGE_OPTIONS } from 'src/app/core/constants/shared.constant';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-branch',
@@ -9,7 +10,6 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./branch.component.scss']
 })
 export class BranchComponent implements OnInit {
-
   breadCrumbItems = [
     { label: 'Quản trị hệ thống' },
     { label: 'Quản lý chi nhánh', active: true }
@@ -29,10 +29,24 @@ export class BranchComponent implements OnInit {
 
   DEFAULT_PER_PAGE_OPTIONS = DEFAULT_PER_PAGE_OPTIONS;
 
-  constructor(private systemService: SystemService, private modalService: BsModalService) {}
+  branchForm!: FormGroup;
+  submitted = false;
+
+  @ViewChild('createModal') createTemplateRef!: TemplateRef<any>;
+
+  constructor(
+    private systemService: SystemService,
+    private modalService: BsModalService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.getItems();
+    this.branchForm = this.fb.group({
+      code: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      name: ['', [Validators.required]],
+      address: ['', [Validators.required]]
+    });
   }
 
   get searchParams(): any {
@@ -70,5 +84,25 @@ export class BranchComponent implements OnInit {
   openDetailModal(template: TemplateRef<any>, item: any): void {
     this.selectedItem = item;
     this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+  }
+
+  submitBranchForm(): void {
+    this.submitted = true;
+    if (this.branchForm.invalid) return;
+
+    const payload = this.branchForm.value;
+    console.log('Submitted data:', payload);
+
+    // Giả lập lưu → đóng modal và reset form
+    this.modalRef?.hide();
+    this.branchForm.reset();
+    this.submitted = false;
+    this.getItems();
+  }
+
+  openCreateModal(): void {
+    this.submitted = false;
+    this.branchForm.reset();
+    this.modalRef = this.modalService.show(this.createTemplateRef, { class: 'modal-lg' });
   }
 }
