91787a15cc2b7ff7d4654f5a385e13ceecd28d62
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-11-11 00:47:36 +0700

    Add no space

--- src/app/shared/directives/upper-noaccent.directive.ts
+++ src/app/shared/directives/upper-noaccent.directive.ts
@@ -1,15 +1,22 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appUpperNoAccent]'
 })
 export class UpperNoAccentDirective {
+  @Input() noSpace: boolean = false;
+
   constructor(private el: ElementRef<HTMLInputElement>) {}
 
   @HostListener('input', ['$event'])
   onInput(event: Event) {
     const input = this.el.nativeElement;
     let value = input.value;
+
+    // Xóa dấu cách nếu noSpace được bật
+    if (this.noSpace) {
+      value = value.replace(/\s/g, '');
+    }
 
     // Chuẩn hóa chuỗi và xóa dấu Tiếng Việt
     const normalized = value
@@ -19,10 +26,18 @@ export class UpperNoAccentDirective {
       .replace(/Đ/g, 'D')
       .toUpperCase();
 
-    if (value !== normalized) {
+    if (input.value !== normalized) {
       const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value')?.set;
       nativeInputValueSetter?.call(input, normalized);
       input.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    // Ngăn chặn nhập dấu cách nếu noSpace được bật
+    if (this.noSpace && event.key === ' ') {
+      event.preventDefault();
     }
   }
 }
